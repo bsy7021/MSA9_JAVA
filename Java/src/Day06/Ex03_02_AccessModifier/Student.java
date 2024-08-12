f2b1991872016e601199a0b5f6e4e670aa9a8bbf
bsy7021 <bsy7021@naver.com>
2024-08-12 17:42:38 +0900

    2024 / 08 / 12

---
+++ Java/src/Day06/Ex03_02_AccessModifier/Student.java
@@ -0,0 +1,27 @@
+package Day06.Ex03_02_AccessModifier;
+
+//다른 패키지에 있는 클래스를 가져오려면 항상 import 해줘야 함
+import Day06.Ex03_01_AccessModifier.Person;
+
+public class Student extends Person {
+
+	//생성자
+	public Student() {
+		super();
+	}
+
+	public Student(String name, int age, int height, int weight) {
+		super(name, age, height, weight);
+	}
+	
+	//메소드
+	public void defualtSetting() {
+		name = "황상건";	//public 접근지정자로, 모든 곳에서 접근 가능
+		age = 31;		//protected 접근지정자로, Person 상속받았기 때문에 다른 패키지라도 접근 가능
+		//heigth = 170;	//default 접근지정자로, 같은 패키지에서만 접근 가능
+		//weight = 100;	//private 접근지정자로, 해당 클래스에서만 접근 가능
+		setHeight(170);
+		setWeight(100);
+	}
+	
+}
